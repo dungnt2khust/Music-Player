ba6c58281cafeaaa3a6b66a0af206e188a34928f
Dung <dunggamo2000@gmail.com>
2021-07-05 12:03:49 +0700

    1. Render songs and 2. Srcoll top

---
+++ app.js
@@ -0,0 +1,123 @@
+/**
+ * 1. Render songs
+ * 2. Scroll top
+ * 3. Play / Pause / Seek
+ * 4. CD rotate
+ * 5. Next / prev
+ * 6. Random
+ * 7. Next / Repeat when ended
+ * 8. Active song
+ * 9. Scroll active song into view
+ * 10. Play song when click
+ */
+
+const $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
+
+const app = {
+  songs: [
+    {
+      name: 'Nevada',
+      singer: 'Vicetone',
+      path: 'https://aredir.nixcdn.com/NhacCuaTui924/Nevada-Vicetone-4494556.mp3?st=_IjpS9u0LjapNgzm058wVw&e=1623143773',
+      image: 'https://i.pinimg.com/originals/f8/6f/33/f86f3378e656883b33594f06d78d1634.jpg',
+    },
+    {
+      name: 'Light It Up',
+      singer: 'Robin Hustin x TobiMorrow',
+      path: 'https://aredir.nixcdn.com/NhacCuaTui968/LightItUp-RobinHustinTobimorrowJex-5619031.mp3?st=kzpVQ5kKnf2LlcAqM6lnxg&e=1623143881',
+      image: 'https://avatar-ex-swe.nixcdn.com/song/2019/01/08/1/3/d/a/1546913843457_640.jpg',
+    },
+    {
+      name: 'Yoru ni kakeru',
+      singer: 'YOASOBI',
+      path: 'https://aredir.nixcdn.com/NhacCuaTui992/YoruNiKakeru-YOASOBI-6149490.mp3?st=68hnFhtGF6RukKDcDcW9Mw&e=1623132179',
+      image: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/16788ee5-3436-474a-84fd-6616063a1a9a/de2f4eq-bc67fa17-8dae-46a9-b85d-fe8082c34841.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzE2Nzg4ZWU1LTM0MzYtNDc0YS04NGZkLTY2MTYwNjNhMWE5YVwvZGUyZjRlcS1iYzY3ZmExNy04ZGFlLTQ2YTktYjg1ZC1mZTgwODJjMzQ4NDEucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.dABuqANeQEs6FBfslZHdG1lW_gDwzf61yqiSABROSx0',
+    },
+    {
+      name: 'Muộn rồi mà sao còn',
+      singer: 'Sơn Tùng M-TP',
+      path: 'https://aredir.nixcdn.com/Believe_Audio19/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3?st=w9AA-eyRI7yD_VYGfvVWeQ&e=1623141624',
+      image: 'https://pbs.twimg.com/media/Ez5jRyVVgAQN6Kh.jpg',
+    },
+    {
+      name: 'See You Again',
+      singer: 'Charlie Puth ft Wiz Khalifa',
+      path: 'https://aredir.nixcdn.com/NhacCuaTui894/SeeYouAgain-KurtSchneiderEppicAlexGoot-3888930.mp3?st=1q73myBS8FKr8Rx0snpMJw&e=1623144094',
+      image: 'https://nghiennhac.com/wp-content/uploads/2020/09/see-you-again-0.jpg',
+    },
+    {
+      name: 'Shape of You',
+      singer: 'Ed Sheeran',
+      path: 'https://aredir.nixcdn.com/NhacCuaTui945/ShapeOfYou-AlexGootAndieCase-5076956.mp3?st=9I9Z2TBGWNOnQRfIJDomDA&e=1623138210',
+      image: 'https://is2-ssl.mzstatic.com/image/thumb/Music122/v4/09/a0/64/09a0641c-e5fa-407e-9829-47702358ec72/190295819972.jpg/1200x1200bf-60.jpg',
+    }
+    ,
+    {
+      name: 'Symphony',
+      singer: 'Clean Bandit',
+      path: 'https://aredir.nixcdn.com/Sony_Audio37/Symphony-CleanBanditZaraLarsson-4822950.mp3?st=sPgJSXtRXYpT_rznXyez6g&e=1623144426',
+      image: 'https://i.ytimg.com/vi/PIf9GvWaxQQ/maxresdefault.jpg',
+    },
+    {
+      name: 'Waiting For Love',
+      singer: 'Avicii',
+      path: 'https://aredir.nixcdn.com/Unv_Audio45/WaitingForLove-Avicii-4203283.mp3?st=mXGv6kIqbxg_coAyUqzlnw&e=1623144462',
+      image: 'https://i.ytimg.com/vi/Hmbm3G-Q444/maxresdefault.jpg',
+    },
+    {
+      name: 'Alone',
+      singer: 'Marshmello',
+      path: 'https://aredir.nixcdn.com/NhacCuaTui927/Alone-Marshmello-4456939.mp3?st=RTsMC9tNcKEi8fd0iKtdaA&e=1623144502',
+      image: 'https://i.ytimg.com/vi/UNB8F0ObA4g/maxresdefault.jpg',
+    },
+    {
+      name: 'Something Just Like This',
+      singer: 'The Chainsmokers & Coldplay',
+      path: 'https://aredir.nixcdn.com/Sony_Audio39/SomethingJustLikeThis-TheChainsmokersColdplay-5337136.mp3?st=VQuH6VgNsPlBizbk-c7n3w&e=1623144556',
+      image: 'https://avatar-ex-swe.nixcdn.com/song/2017/11/07/a/1/4/5/1510038809679_640.jpg',
+    },
+    {
+      name: 'Sugar',
+      singer: 'Maroon 5',
+      path: 'https://aredir.nixcdn.com/Unv_Audio73/Sugar-Maroon5-3338455.mp3?st=3FUWEyikJePPeAuREUcw9g&e=1623144644',
+      image: 'https://i.ytimg.com/vi/7vw84EkHOlY/maxresdefault.jpg',
+    },
+  ],
+
+  render: function() {
+    const htmls = this.songs.map(songs => {
+      return `
+          <div class="song">
+            <div class="thumb"
+              style="background-image: url('${songs.image}')">
+            </div>
+            <div class="body">
+              <h3 class="title">${songs.name}</h3>
+              <p class="author">${songs.singer}</p>
+            </div>
+            <div class="option">
+              <i class="fas fa-ellipsis-h"></i>
+            </div>
+          </div>`;
+    });
+    $('.playlist').innerHTML = htmls.join('');
+  },
+  handleEvents: function() {
+    const cd = $('.cd');
+    const cdWidth = cd.offsetWidth;
+    document.onscroll = function() {
+      const scrollTop =  window.scrollY;
+      const newCdWidth = cdWidth - scrollTop;
+      cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0;
+      cd.style.opacity = newCdWidth / cdWidth;
+    }
+  },
+  start: function() {
+    this.handleEvents();
+    this.render();
+  }
+};
+
+
+app.start();
